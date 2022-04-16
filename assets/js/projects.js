0c06187d86013b04a7ed7ee8aa0439ff39b90411
Hadestia <89353591+Hadestia@users.noreply.github.com>
2022-04-17 02:07:41 +0800

    Update projects.js

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -2,7 +2,7 @@
 
 const addFilters = (name, data) => {
     let filter = '<li data-filter=".filter-' + data +'">' + name + '</li>';
-    $('#portfolio-filters').append(filter);
+    $('#portfolio-flters').append(filter);
 }
 
 const addProject = (obj) => {
@@ -12,24 +12,25 @@ const addProject = (obj) => {
         let project = '';
         let alt = obj.alt || '';
         let section = obj.section;
-        let preview = obj.preview || true;
+        let forward = obj.forward || true;
         let title = obj.title || 'Untitled';
-        let link = obj.link || 'details-grahics.html';
+        let link = obj.link || 'portfolio-details.html';
         let source = 'assets/img/portfolio/' + obj.source;
         
-        project += '<div class="col-lg-4 col-md-6 portfolio-item filter-' + section +'">';
-        project += '<div class="portfolio-wrap center-img">';
+        project += '<div class="col-lg-4 col-md-6 portfolio-item filter-' + section + '"><div class="portfolio-wrap">';
         project += '<img src="' + source + '" class="img-fluid" alt="' + alt + '">';
         project += '<div class="portfolio-links">';
-        project += '<a href="' + source + '" data-gallery="portfolioGallery" class="portfolio-lightbox" title="' + title + '"><i class="bx bx-plus"></i></a>';
-        project += '<a href="' + link + '" title="More Details"><i class="bx bx-link"></i></a> </div>';
-        project += '</div></div>';
+        //project += '<a href="' + source + '" data-gallery="portfolioGallery" class="portfolio-lightbox" title="' + title + '"><i class="bx bx-plus"></i></a>';
+        if (forward) {
+            project += '<a href="' + link + '" title="More Details"><i class="bx bx-link"></i></a>';
+        }
+        project += '</div></div></div>';
         
-        $('#projects').append(project);
+        $('.portfolio-container').append(project);
     }
 }
 
-$(document).ready(function () {
+$(document).ready(function() {
     //project controller
     $.getJSON("assets/json/portfolio.json", 
     function (data) {
